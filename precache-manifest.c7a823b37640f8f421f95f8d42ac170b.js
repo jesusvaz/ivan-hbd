f3a2b6b86fc0af26042534ded432c6d89b215996
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-09-18 14:51:01 -0600

    Updates

---
+++ precache-manifest.c7a823b37640f8f421f95f8d42ac170b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3372742fb2993696eadb85c74617cf17",
+    "url": "/ivan-hbd/index.html"
+  },
+  {
+    "revision": "a2c58b5913a25f88b187",
+    "url": "/ivan-hbd/static/css/main.47f666f0.chunk.css"
+  },
+  {
+    "revision": "13dfced3db2c638529a8",
+    "url": "/ivan-hbd/static/js/2.2268a77b.chunk.js"
+  },
+  {
+    "revision": "a2c58b5913a25f88b187",
+    "url": "/ivan-hbd/static/js/main.b730fcc5.chunk.js"
+  },
+  {
+    "revision": "9176fe257b7f0bd7f0eb",
+    "url": "/ivan-hbd/static/js/runtime~main.672c336d.js"
+  },
+  {
+    "revision": "636ad66059ad6dd8f30949010412fe1d",
+    "url": "/ivan-hbd/static/media/ivanCartoon.636ad660.jpg"
+  }
+]);
